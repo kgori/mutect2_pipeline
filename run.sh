66ef176b9f131a9f3eafe7fdde2f7ab3a9a9f3af
Kevin Gori <kcg25@cam.ac.uk>
2025-09-25 15:32:45 +0000

    Add flowchart generation to run script

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ nextflow run main.nf \
          --reference /lustre/scratch125/casm/staging/team267_murchison/kg8/20250704_Mutect2/refs/Canis_lupus_familiaris.CanFam3.1.dna.toplevel.fa \
          --normals /lustre/scratch125/casm/staging/team267_murchison/kg8/20250704_Mutect2/bams/normals \
          --tumours /lustre/scratch125/casm/staging/team267_murchison/kg8/20250704_Mutect2/bams/tumours \
-         --outdir /lustre/scratch125/casm/staging/team267_murchison/kg8/20250704_Mutect2/results_16_samples_20250903 \
-         -resume
+         --outdir /lustre/scratch125/casm/staging/team267_murchison/kg8/20250704_Mutect2/results_16_samples_20250911 \
+         -resume \
+         -with-dag flowchart.html
